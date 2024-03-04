4d9cf608c59fc56b8ac61df273e4892de4f564af
RezaAbedy <reza.abedy75@gmail.com>
2024-03-04 11:29:51 +0330

    Neural_Network_Class

---
+++ Neural_Network_Class/Use_NN_Class.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from NN_Class import NeuralNetwork
+
+input_vector = np.array([1.66, 1.56])
+learning_rate = 0.1
+
+neurak_network = NeuralNetwork(learning_rate)
+
+prediction = neurak_network.predict(input_vector)
+
+print(f"Weight = {neurak_network.weights}, bias = {neurak_network.bias}")
+print(f"Prediction = {prediction}")
+
+
+input_vectors = np.array([
+    [3, 1.5],
+    [2, 1],
+    [4, 1.5],
+    [3, 1],
+    [3.5, 0.5],
+    [2, 0.5],
+    [5.5, 1],
+    [1, 1]
+])
+
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+
+learning_rate = 0.1
+
+neural_network = NeuralNetwork(learning_rate)
+
+print(f"init => weights: {neural_network.weights}, bias: {neural_network.bias}")
+
+training_error = neural_network.train(input_vectors, targets, 10000)
+
+print(f"after train => weights: {neural_network.weights}, bias: {neural_network.bias}")
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
+plt.show()
